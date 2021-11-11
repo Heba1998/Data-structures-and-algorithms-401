673ec0dced9145db96f63dd2c18e92af91482ccb
Heba1998 <hebaalmomani1998@gmail.com>
2021-11-11 18:31:53 +0200

    Exam training

--- hash/src/main/java/Challenge34.java
+++ hash/src/main/java/Challenge34.java
@@ -5,19 +5,25 @@ public class Challenge34 {
     {HashSet<Character> charSet = new HashSet<>();
         for(int i = 0; i< str.length();i++) {
             charSet.add(str.charAt(i));
-        }if(charSet.size() == str.length()) {
+        }if (charSet.size() == str.length()) {
             return true;
         } else {
             return false;
         }
     }
+
+
+
     public static void main(String[] args)
     {
         String str = "I love cats ";
         String string = str.replace(" ", "").toLowerCase();
-        if (uniquecharacters(string)) {
+        if (uniquecharacters(string))
+        {
             System.out.print("true");
-        }else{
+        }
+        else
+        {
             System.out.print("False");
         }
     }
